afe1ac0953246322aa0562a060cf49c613c7d636
Mehrdata <mehrdaad.jafari@gmail.com>
2023-03-21 19:25:30 +0330

    Improved UI
    Dark and Light Mode added
    Review the CSS code

--- index.js
+++ index.js
@@ -37,3 +37,31 @@ function reset(){
 }
 
 
+function lightMod() {
+var backGroundColor = document.body;
+backGroundColor.classList.toggle("light-mode");
+
+var countNumber = document.getElementById("counter")
+countNumber.classList.toggle("h2-light")
+
+var navIcons = document.getElementById("navbar");
+navIcons.classList.toggle("navbar-light");
+
+var btnHolderInc = document.getElementById("inc-btn");
+btnHolderInc.classList.toggle("btn-light")
+
+var btnHolderDec = document.getElementById("dec-btn");
+btnHolderDec.classList.toggle("btn-light")
+
+var textBox = document.getElementById("text-box1")
+textBox.classList.toggle("text-section-light")
+
+var textBox2 = document.getElementById("text-box2")
+textBox2.classList.toggle("text-section-light")
+
+var saveBtn = document.getElementById("save-btn")
+saveBtn.classList.toggle("save-round-btn-light")
+}
+
+
+
